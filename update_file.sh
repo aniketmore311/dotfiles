b9cb849f97a0ba51209437ac5f29b57138347740
aniket <aniketavinashmore343@gmail.com>
2022-04-28 14:05:45 +0530

    add zsh files

---
+++ update_file.sh
@@ -0,0 +1,2 @@
+cp ~/.zshrc ./.zshrc
+cp ~/.zshplugins/zshplugins.zsh ./zshplugins.zsh
---
+++ zshplugins.zsh
@@ -0,0 +1,6 @@
+# source ./zsh-autosuggestinos/zsh-autosuggestions.zsh
+PLUGINS_DIR=~/.zshplugins
+# config
+ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=245"
+# source $PLUGINS_DIR/test.zsh
+source $PLUGINS_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
