62fbb9b58b446fdc11863f9f2d134920f4da679c
Aniket More <63508109+aniketmore311@users.noreply.github.com>
2022-04-30 11:41:05 +0530

    update postinstall.sh

--- postinstall.sh
+++ postinstall.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -ex
 
+sudo apt update
+
+# tools
 sudo apt install git -y
+sudo apt install curl -y
+sudo apt install build-essential -y
+
+# environments
 sudo apt install python3 -y
 sudo apt install python3-pip -y
 sudo apt install default-jdk -y
 sudo apt install default-jre -y
+# nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
 
 # snap packages
 sudo snap install code --classic -y
