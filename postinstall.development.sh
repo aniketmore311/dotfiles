5e83159da48b71b78bc81cfb0ce8ba66f16b7f20
Aniket More <63508109+aniketmore311@users.noreply.github.com>
2022-05-07 12:53:10 +0530

    remove docker installation

--- postinstall.development.sh
+++ postinstall.development.sh
@@ -7,18 +7,6 @@ sudo apt update
 # vscode
 sudo snap install code --classic
 
-# docker
-sudo apt-get remove docker docker-engine docker.io containerd runc -y
-sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg lsb-release -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-
-sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-
 # aws cli
 wget -O awscliv2.zip "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip"
 unzip awscliv2.zip
